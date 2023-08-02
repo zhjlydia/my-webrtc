45f3c28f866f727666d4f061be429d955ef2744a
Lydia Zhou <lydia.zhou@ringcentral.com>
2023-08-02 10:55:05 +0800

    page

--- src/utils/media.ts
+++
@@ -1,7 +0,0 @@
-export const getLocalStream = (constraints: MediaStreamConstraints) => {
-    return navigator.mediaDevices.getUserMedia(constraints);
-};
-
-export const getDisplayMedia = (options: DisplayMediaStreamOptions) => {
-    return navigator.mediaDevices.getDisplayMedia(options);
-};
